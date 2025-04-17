c1aa23ecc8f1061ec1479c3c027193b640d660f4
cfsen <103598332+cfsen@users.noreply.github.com>
2025-04-18 00:55:17 +0200

    Add raylib for basic visual demo

--- Program.cs
+++ Program.cs
@@ -1,7 +1,56 @@
 ﻿using BoneUtils.Tests;
 using BoneUtils.Entity.Skeleton;
+using Raylib_cs;
+using System.Numerics;
 
-SkeletonEntityTests bt = new();
-bt.Entity_TranslateSpine();
-bt.Entity_RotateSpine();
-bt.Entity_WorldTranslateVectors();
+//SkeletonEntityTests bt = new();
+//bt.Entity_TranslateSpine();
+//bt.Entity_RotateSpine();
+//bt.Entity_WorldTranslateVectors();
+//return;
+
+RayLibVisualTest rlvt = new();
+SkeletonEntity spine = rlvt.SimpleSpine();
+Quaternion q = Quaternion.CreateFromYawPitchRoll(0.0f, MathF.PI/2, 0.0f);
+
+Raylib.InitWindow(800, 600, "3D Test");
+Raylib.SetTargetFPS(60);
+
+Camera3D camera = new Camera3D {
+    Position = new Vector3(6.0f, 2.0f, 0.0f),
+    Target = new Vector3(0.0f, 2.0f, 0.0f),
+    Up = new Vector3(0.0f, 1.0f, 0.0f),
+    FovY = 60.0f,
+    Projection = CameraProjection.Perspective
+};
+
+while (!Raylib.WindowShouldClose()) {
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Black);
+    Raylib.BeginMode3D(camera);
+
+	foreach(var bone in spine.Bones.Values) {
+		Raylib.DrawSphere(spine.BoneWorldPosition(bone), 0.25f, Color.Red);
+	}
+
+    Raylib.EndMode3D();
+	Raylib.DrawFPS(10, 10);
+
+	if(Raylib.IsKeyPressed(KeyboardKey.One)) {
+		spine.RootNode.Rotate(q);
+	}
+	if(Raylib.IsKeyPressed(KeyboardKey.Two)) {
+		spine.Bones["SpineA"].Rotate(q);
+	}
+	if(Raylib.IsKeyPressed(KeyboardKey.Three)) {
+		spine.Bones["SpineB"].Rotate(q);
+	}
+	if(Raylib.IsKeyPressed(KeyboardKey.Four)) {
+		spine.Bones["SpineC"].Rotate(q);
+	}
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
+
---
+++ Tests/RayLibVisualTest.cs
@@ -0,0 +1,12 @@
+﻿using BoneUtils.Entity.Skeleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoneUtils.Tests;
+public class RayLibVisualTest :MockDataBuilder{
+	public SkeletonEntity SimpleSpine() => Mock_Spine();
+}
+
