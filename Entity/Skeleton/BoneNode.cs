9f38ff562b9ada4937c8267c84c685fe5dda7aee
cfsen <103598332+cfsen@users.noreply.github.com>
2025-04-18 00:54:12 +0200

    Shift pivot point for correct rotation of child bones.

--- Entity/Skeleton/BoneNode.cs
+++ Entity/Skeleton/BoneNode.cs
@@ -1,4 +1,5 @@
 ﻿using BoneUtils.Helpers;
+using System.Diagnostics;
 using System.Numerics;
 
 namespace BoneUtils.Entity.Skeleton;
@@ -19,19 +20,25 @@ public class BoneNode {
 	public bool Branching => Children.Count > 1;
 	public void Translate(Vector3 offset) {
 		Transform.Position += offset;
-		foreach (var child in Children) {
+		foreach (var child in Children) 
 			child.Value.Translate(offset);
-		}
 	}
-	public void Rotate(Quaternion rotation, Func<Vector3, Quaternion, Vector3>? positionTransformer = null) {
+	public void Rotate(
+		Quaternion rotation,
+		Func<Vector3, Quaternion, Vector3>? positionTransformer = null,
+		Vector3? origin = null) {
+
 		if (positionTransformer != null)
-			Transform.BatchRotatePropagation(positionTransformer(Transform.Position, rotation), rotation);
+			Transform.BatchRotatePropagation(
+				positionTransformer(Transform.Position-origin ?? Vector3.Zero, rotation) + origin ?? Vector3.Zero,
+				rotation);
 		else {
+			origin = Transform.Position;
 			Transform.Rotation = Quaternion.Normalize(Transform.Rotation * rotation);
 			positionTransformer = MathHelper.RotateWithDriftCorrection;
 		}
 
 		foreach (var child in Children.Values)
-			child.Rotate(rotation, positionTransformer);
+			child.Rotate(rotation, positionTransformer, origin);
 	}
 }
