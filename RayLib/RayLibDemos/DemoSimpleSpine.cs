ef3b4c2b55c5eb090354b6f28f017b2c65233846
cfsen <103598332+cfsen@users.noreply.github.com>
2025-04-20 11:58:25 +0200

    Animate raylib spine demo

--- RayLib/RayLibDemos/DemoSimpleSpine.cs
+++ RayLib/RayLibDemos/DemoSimpleSpine.cs
@@ -1,5 +1,7 @@
 ﻿using BoneUtils.Entity.Skeleton;
+using BoneUtils.Helpers;
 using Raylib_cs;
+using System.Diagnostics;
 using System.Numerics;
 using System.Reflection.Metadata;
 
@@ -9,17 +11,18 @@ public class DemoSimpleSpine :DemoBase {
 	private SkeletonEntity Spine;
 	private SkeletonEntityOps SkelOps;
 	private Quaternion q = Quaternion.CreateFromYawPitchRoll(0.0f, MathF.PI/2, 0.0f);
+	private Quaternion qAnimate = Quaternion.CreateFromYawPitchRoll(MathHelper.DegToRad(0.5f), MathHelper.DegToRad(0.5f), 0.0f);
 
 	public DemoSimpleSpine(SkeletonEntityOps skeops) {
 		SkelOps = skeops;
 		Spine = ConstructSkeleton();
 	}
 
-	public override void DrawHelpOverlay() {
-		Raylib.DrawText("Press 1,2,3,4 to rotate the root, SpineA, SpineB, SpineC bones.", 10, 50, 20, Color.Red);
-	}
 	public override void Draw() {
 		DrawBoneNodeNetwork(Spine);
+	}
+	public override void DrawHelpOverlay() {
+		Raylib.DrawText("Press 1,2,3,4 to rotate the root, SpineA, SpineB, SpineC bones.", 10, 50, 20, Color.Red);
 	}
 	public override void HandleDemoInput() {
 		if(Raylib.IsKeyPressed(KeyboardKey.One))
@@ -31,8 +34,16 @@ public class DemoSimpleSpine :DemoBase {
 		if(Raylib.IsKeyPressed(KeyboardKey.Four))
 			Spin("SpineC", q);
 	}
+	public override void Update(float deltaTime) {
+		AnimateRotation();
+	}
 	private void Spin(string node, Quaternion q) {
 		Spine.Bones[node].Rotate(q);
+	}
+	private void AnimateRotation() {
+		foreach(var bone in Spine.Bones.Values) {
+			bone.Rotate(qAnimate);
+		}
 	}
 	private SkeletonEntity ConstructSkeleton() { 
 		var spine = Mock_Spine();
