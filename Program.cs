158b48737331fd9cbb33b2095d5e8bcf79a59069
cfsen <103598332+cfsen@users.noreply.github.com>
2025-04-21 13:49:37 +0200

    Add tracking peak delta time to Raylib render loop

--- Program.cs
+++ Program.cs
@@ -30,16 +30,21 @@ Camera3D camera = new Camera3D {
     Position = new Vector3(6.0f, 2.0f, 0.0f),
     Target = new Vector3(0.0f, 2.0f, 0.0f),
     Up = new Vector3(0.0f, 1.0f, 0.0f),
-    FovY = 60.0f,
+    FovY = 45.0f,
     Projection = CameraProjection.Perspective
 };
+
 float deltaTime = 0.0f;
+float deltaTimeHigh = 0.0f;
+
 while (!Raylib.WindowShouldClose()) {
 	rlvt.HandleInput();
 	rlvt.ActiveDemo.HandleDemoInput();
 
 	deltaTime = Raylib.GetFrameTime();
 	rlvt.ActiveDemo.Update(deltaTime);
+
+	if(deltaTime > deltaTimeHigh) deltaTimeHigh = deltaTime;
 
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.Black);
@@ -49,7 +54,8 @@ while (!Raylib.WindowShouldClose()) {
     Raylib.EndMode3D();
 
 	Raylib.DrawFPS(10, 10);
-	//Raylib.DrawText($"dt: {deltaTime}", 100, 10, 15, Color.Green);
+	Raylib.DrawText($"dt: {deltaTime}", 100, 10, 16, Color.Green);
+	Raylib.DrawText($"high: {deltaTimeHigh}", 100, 26, 16, Color.Green);
 	rlvt.DrawHelp();
 	rlvt.DrawHelpOverlay();
 
