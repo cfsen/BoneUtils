ab741b0498e2e26a419aa6f3993848c37ebd9354
cfsen <103598332+cfsen@users.noreply.github.com>
2025-04-26 10:33:26 +0200

    Add debug vector draw for quat orientation to demo base; split BoneNode point and connector draw

--- BoneUtils/RayLib/RayLibDemos/DemoBase.cs
+++ BoneUtils/RayLib/RayLibDemos/DemoBase.cs
@@ -6,39 +6,74 @@ using System.Numerics;
 
 namespace BoneUtils.RayLib.RayLibDemos;
 public abstract class DemoBase :MockDataBuilder, IDemo{
-	public virtual void Draw() { }
-	public virtual void DrawHelpOverlay() { }
+
+	// Interface contracts
+
+	public virtual void Draw3D() { }
+	public virtual void Draw2D() { }
 	public virtual void HandleDemoInput() { }
 	public virtual void Update(float deltaTime) { }
-	internal void DrawBoneNodeNetwork(SkeletonEntity sken) {
-		for(var i = 0; i < sken.RenderBoneCount; i++) {
+
+	// Skeleton visualizers
+
+	internal void DrawBoneNodeNetwork(SkeletonEntity sken, bool drawConnectors = true) {
+		DrawBoneNode(sken);
+		if(drawConnectors) 
+			DrawBoneNodeConnectors(sken);
+	}
+	internal void DrawBoneNode(SkeletonEntity sken) {
+		for(var i = 0; i < sken.RenderBoneCount; i++)
 			Raylib.DrawSphere(sken.BoneWorldPosition(sken.RenderBones[i]), 0.20f, Color.Red);
-			if(sken.RenderBones[i].ParentBone != null)
+	}
+	internal void DrawBoneNodeConnectors(SkeletonEntity sken) {
+		for(var i = 0; i < sken.RenderBoneCount; i++) {
+			if (sken.RenderBones[i].ParentBone != null)
 				Raylib.DrawLine3D(
-					sken.BoneWorldPosition(sken.RenderBones[i].ParentBone!), 
+					sken.BoneWorldPosition(sken.RenderBones[i].ParentBone!),
 					sken.BoneWorldPosition(sken.RenderBones[i]),
 					Color.Red);
 		}
+	}
+	internal void DrawQuaternionOrientation(SkeletonEntity sken) {
+		Dictionary<string,Vector3> indicator = [];
+		// TODO translation into world space
+		for(var i = 0; i < sken.RenderBoneCount; i++) {
+			indicator = MathHelper.CreateLocalDirectionVectors(
+				sken.RenderBones[i].Transform.Rotation, 
+				sken.RenderBones[i].Transform.Position);
+
+			Raylib.DrawLine3D(sken.RenderBones[i].Transform.Position, indicator["X"], Color.Orange);
+			Raylib.DrawLine3D(sken.RenderBones[i].Transform.Position, indicator["Y"], Color.Green);
+			Raylib.DrawLine3D(sken.RenderBones[i].Transform.Position, indicator["Z"], Color.Blue);
+		}
+	}
+
+	// Text overlay
+
+	internal void DrawQuatDebug(BoneNode node) {
+		List<string> dbgLines = [];
+		var euler = MathHelper.QuaternionToEuler(node.Transform.Rotation.ToQuaternion());
+
+		dbgLines.Add($"Quat: {node.Name}");
+		dbgLines.Add($"W: {node.Transform.Rotation.W}");
+		dbgLines.Add($"X: {node.Transform.Rotation.X}");
+		dbgLines.Add($"Y: {node.Transform.Rotation.Y}");
+		dbgLines.Add($"Z: {node.Transform.Rotation.Z}");
+
+		dbgLines.Add("");
+
+		dbgLines.Add("euler:");
+		dbgLines.Add($"X: {MathHelper.RadToDeg(euler.X)}");
+		dbgLines.Add($"Y: {MathHelper.RadToDeg(euler.Y)}");
+		dbgLines.Add($"Z: {MathHelper.RadToDeg(euler.Z)}");
+
+		DrawDebug(dbgLines);
 	}
 	internal void DrawDebug(List<string> dbgLines) {
 		int i = 0;
 		foreach(var dbgLine in dbgLines) {
 			Raylib.DrawText(dbgLine, 10, 100+i*18, 16, Color.White);
 			i++;
-		}
-	}
-	internal void DrawQuaternionOrientation(SkeletonEntity sken) {
-		// TODO improve visual clarity
-		Vector3 pos = Vector3.Zero;
-		Vector3 ang = Vector3.Zero;
-		List<Vector3> directionals = [];
-		for(var i = 0; i < sken.RenderBoneCount; i++) {
-			pos = sken.BoneWorldPosition(sken.RenderBones[i]);
-			ang = MathHelper.QuaternionToEuler(sken.RenderBones[i].Transform.Rotation);
-
-			Raylib.DrawCircle3D(pos, 0.30f, Vector3.UnitX, MathHelper.RadToDeg(ang.X), Color.Orange);
-			Raylib.DrawCircle3D(pos, 0.30f, Vector3.UnitY, MathHelper.RadToDeg(ang.Y), Color.Green);
-			Raylib.DrawCircle3D(pos, 0.30f, Vector3.UnitZ, MathHelper.RadToDeg(ang.Z), Color.Blue);
 		}
 	}
 }
