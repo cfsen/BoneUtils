045cabb80ebebef45001cf7ae16fff65de67904d
cfsen <103598332+cfsen@users.noreply.github.com>
2025-05-01 10:01:48 +0200

    Add transform buffering to BoneNode

--- BoneUtils/Entity/Skeleton/BoneNode.cs
+++ BoneUtils/Entity/Skeleton/BoneNode.cs
@@ -8,6 +8,7 @@ public class BoneNode {
 	public string Name;
 	public BoneNode? ParentBone;
 	public SkeletonEntity? ParentEntity;
+	public ParentRelativePosition? ParentRelativePosition;
 
 	public int? TreeDepth;
 	public Dictionary<string, BoneNode> Children = [];
@@ -15,14 +16,7 @@ public class BoneNode {
 	public int RenderChildrenCount = 0;
 
 	public Transform Transform;
-	public ParentRelativePosition? ParentRelativePosition;
-
-	public BoneNode(string name, Transform transform, BoneNode? parent = null, Dictionary<string, BoneNode>? children = null, SkeletonEntity? parentEntity = null) {
-		Name = name;
-		ParentBone = parent;
-		Transform = transform;
-		Children = children ?? [];
-	}
+	public BoneNodeXfmBuffer TransformBuffer;
 
 	public delegate Vector3 RotateXfmHandler(BoneNode node, Vector3 nodePosition, Quat newOrientation, Vector3 origin); 
 	public delegate Matrix4x4 SetXfmHandler(BoneNode node, List<(BoneNode, Transform)>? Transforms = null);
@@ -30,10 +24,40 @@ public class BoneNode {
 	public bool Branching => Children.Count > 1;
 	public bool HasChildren => Children.Count > 0;
 
+	public BoneNode(string name, Transform transform, BoneNode? parent = null, Dictionary<string, BoneNode>? children = null,
+				 SkeletonEntity? parentEntity = null) {
+		Name = name;
+		ParentBone = parent;
+		Transform = transform;
+		Children = children ?? [];
+		TransformBuffer = new BoneNodeXfmBuffer();
+	}
+
+
 	public void Translate(Vector3 offset) {
 		Transform.Position += offset;
 		foreach (var child in Children.Values) 
 			child.Translate(offset);
+	}
+	public bool PrepareTransformBuffer() {
+		if(TransformBuffer.Active) return false;
+		
+		TransformBuffer.Translation = Transform.Position;
+		TransformBuffer.Rotation = Transform.Rotation;
+		TransformBuffer.Scale = Transform.Scale;
+
+		return true;
+	}
+	public bool ApplyTransformBuffer() {
+		if(!TransformBuffer.Complete) return false;
+
+		Transform.Position = TransformBuffer.Translation;
+		Transform.Rotation = TransformBuffer.Rotation;
+		Transform.Scale = TransformBuffer.Scale;
+
+		TransformBuffer.Reset();
+
+		return true;
 	}
 	public void Rotate(Quat rotation, RotateXfmHandler? xfmHandler = null, Vector3? origin = null) {
 		origin ??= Transform.Position;
---
+++ BoneUtils/Entity/Skeleton/BoneNodeXfmBuffer.cs
@@ -0,0 +1,78 @@
+﻿using BoneUtils.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoneUtils.Entity.Skeleton;
+public class BoneNodeXfmBuffer {
+	public Vector3 Translation;
+	public Vector3 Scale;
+	public Quat Rotation;
+	public Matrix4x4 Matrix;
+
+	public bool Active;
+	public bool Complete;
+	public XfmType BufferedXfms;
+
+	public BoneNodeXfmBuffer() {
+		Reset();
+	}
+	public bool Begin() {
+		if(Active || Complete) return false;
+		Reset();
+		Active = true;
+		return true;
+	}
+	public bool Accumulate(Quat Rotation) => Accumulator(Rotation, XfmType.Rotate);
+	public bool Accumulate(Vector3 Translation) => Accumulator(Translation, XfmType.Translate);
+	public bool Accumulate(Matrix4x4 Matrix) => Accumulator(Matrix, XfmType.Matrix);
+	public bool AccumulateScale(Vector3 Scale) => Accumulator(Scale, XfmType.Scale);
+	private bool Accumulator(object xfm, XfmType type) {
+		if(type == XfmType.None) return false;
+
+		switch (type) {
+			case XfmType.Translate:
+				Translation += (Vector3)xfm;
+				break;
+			case XfmType.Rotate:
+				Rotation *= (Quat)xfm;
+				break;
+			case XfmType.Matrix:
+				Matrix *= (Matrix4x4)xfm;
+				break;
+			case XfmType.Scale:
+				Scale += (Vector3)xfm;
+				break;
+			default:
+				return false;
+		}
+
+		BufferedXfms |= type;
+		return true;
+	}
+	public bool End() {
+		if(!Active) return false;
+		Complete = true;
+		return true;
+	}
+	public void Reset() {
+		Translation = Vector3.Zero;
+		Scale = Vector3.One;
+		Rotation = Quat.Identity();
+		Matrix = Matrix4x4.Identity;
+		BufferedXfms = XfmType.None;
+		Active = false;
+		Complete = false;
+	}
+}
+[Flags]
+public enum XfmType {
+	None = 0,
+	Translate = 1,
+	Rotate = 2,
+	Scale = 4,
+	Matrix = 8
+}
