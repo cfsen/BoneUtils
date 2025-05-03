f162762c3ef5cf7adf79d2f8c30a88441fb1c263
cfsen <103598332+cfsen@users.noreply.github.com>
2025-05-03 10:37:25 +0200

    Update demo boilerplate

--- BoneUtils/RayLib/RayLibDemos/DemoBoilerplate.cs
+++ BoneUtils/RayLib/RayLibDemos/DemoBoilerplate.cs
@@ -5,36 +5,33 @@ using System.Numerics;
 
 namespace BoneUtils.RayLib.RayLibDemos;
 public class DemoBoilerplate :DemoBase {
+	private RaylibRenderer Renderer;
 
 	private SkeletonEntity Spine;
 	private SkeletonEntityOps SkelOps;
 
-	public DemoBoilerplate(SkeletonEntityOps skeops) {
+	public DemoBoilerplate(SkeletonEntityOps skeops, RaylibRenderer renderer) {
 		SkelOps = skeops;
 		Spine = ConstructSkeleton();
+		Renderer = renderer;
+		renderMode = RenderMode.Fancy;
 	}
 	public override void Draw3D() {
-		//foreach(var bone in Spine.Bones.Values)
-		//	Raylib.DrawSphere(Spine.BoneWorldPosition(bone), 0.25f, Color.Red);
+		Render(Spine, Renderer);
 	}
 	public override void Draw2D() {
 		Raylib.DrawText("Helpful hints here!", 10, 50, 20, Color.Red);
 	}
 	public override void HandleDemoInput() {
 		//if(Raylib.IsKeyPressed(KeyboardKey.One))
-		//	Spin("Root", q);
 	}
 	public override void Update(float deltaTime) {
 		
-	}
-	private void Spin(string node, Quat q) {
-		Spine.Bones[node].Rotate(q); // TODO Quat
 	}
 	private SkeletonEntity ConstructSkeleton() { 
 		var sken = Mock_Spine();
 		SkelOps.PreProcessSkeleton(ref sken, [
 			SkelOps.ValidateBoneNodeTree,
-			SkelOps.LabelDepthBoneNodeTree,
 			SkelOps.BoneNodeTreeBuildRenderLists
 			]);
 
