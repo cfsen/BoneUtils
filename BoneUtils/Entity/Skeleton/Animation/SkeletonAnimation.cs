6ad429a6fa6b511c0902faed7b4d4734f58d88db
cfsen <103598332+cfsen@users.noreply.github.com>
2025-05-05 11:32:05 +0200

    Move structs to their own file

--- BoneUtils/Entity/Skeleton/Animation/SkeletonAnimation.cs
+++ BoneUtils/Entity/Skeleton/Animation/SkeletonAnimation.cs
@@ -86,37 +86,3 @@ public class SkeletonAnimation {
 		return (!(origin == -1 || target == -1), origin, target);
 	}
 }
-public struct BonesAnimation {
-	public required List<BoneAnimationKeyframe> Keyframes; 
-	public required List<AnimationBlend> FrameBlends;
-	public required float TotalDuration; // Complete duration of animation, in seconds
-	public required BonesAnimationType Type; // should transforms be treated as absolute, or relative to current position (canned anim. vs. procedural)
-}
-public enum BonesAnimationType {
-	Static,
-	Relative,
-	Custom // prep for IoC 
-}
-public struct BoneAnimationKeyframe {
-	public required BoneNode Bone;
-	public required Transform TransformState;
-	public required float TimelinePosition; // Relative to TotalDuration, 0 = at the beginning of the animation, = Duration at the end
-}
-public struct AnimationBlend {
-	public required AnimationBlendType BlendType;
-
-	public required int OriginIndex; // ref to BoneAnimationKeyframe
-	public required int TargetIndex; // ref to BoneAnimationKeyframe
-
-	public required float BlendFactor; // use blendfactor as scalar/exponent/log for linear/exponential/log movement s=vt => v=v*x, v^x, logx(v) etc
-	public required float TimeFactor; // Slow motion essentially. s=vt => t = t*TimeFactor
-	public required float Time; // how long should this take to complete
-}
-public enum AnimationBlendType {
-	None,
-	Linear,
-	Exponential,
-	Logarithmic,
-	Oscillating,
-	Custom // prep for IoC
-}
