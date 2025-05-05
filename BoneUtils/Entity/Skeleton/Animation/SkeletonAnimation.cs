f7c366267893b96d7e97a3687b19097dace59242
cfsen <103598332+cfsen@users.noreply.github.com>
2025-05-05 10:55:17 +0200

    Initial commit for animation system

---
+++ BoneUtils/Entity/Skeleton/Animation/SkeletonAnimation.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoneUtils.Entity.Skeleton.Animation; 
+public class SkeletonAnimation {
+	// Animation owner 
+
+	public required BonesAnimation Animation;
+	public required List<BoneAnimationKeyframe> Keyframes;
+
+	public float TotalDuration; // seconds
+	public bool Loop = true;
+
+	private int KeyframeCount = 0;
+
+	// for sequential lookup optimization
+	private float LastLookupTime = 0;
+	private int LastLookupKeyframe = 1; 
+	private int LastOrigin = -1;
+	private int LastTarget = -1;
+
+	public SkeletonAnimation(BonesAnimation animationContainer) {
+		Animation = animationContainer;
+		Keyframes = animationContainer.Keyframes;
+		TotalDuration = animationContainer.TotalDuration;
+		KeyframeCount = Keyframes.Count;
+		if(KeyframeCount < 2)
+			throw new Exception("Animations must have at least two keyframes."); // TODO find appropriate built in exception
+	}
+	public (bool valid, BoneNode? node, Transform? state) GetKeyframe(float runTime) { 
+		if(runTime > TotalDuration && !Loop) return (false, null, null); 
+		if(Loop && runTime > TotalDuration) runTime %= TotalDuration; // Wrap time around if looping
+
+		// Fetch frames
+		var (valid, origin, target) = CheckLastFrames(runTime);
+		if(!valid) (valid, origin, target) = GetSequentialKeyframes(runTime);
+		if(!valid) (valid, origin, target) = GetKeyframes(runTime);
+		if(!valid) return (false, null, null);
+
+		LastOrigin = origin;
+		LastTarget = target;
+
+		// TODO blend and return current state
+
+		// Just return whichever frame is closer for now
+		if(runTime-Keyframes[origin].TimelinePosition >= Keyframes[target].TimelinePosition-runTime)
+			return (true, Keyframes[target].Bone, Keyframes[target].TransformState);
+		return (true, Keyframes[origin].Bone, Keyframes[origin].TransformState);
+	}
+
+	private (bool valid, int origin, int target) CheckLastFrames(float runTime) {
+		if(LastOrigin < 0 || LastOrigin >= KeyframeCount) return (false, -1, -1);
+		if(LastTarget < 0 || LastTarget >= KeyframeCount) return (false, -1, -1);
+
+		if(Keyframes[LastOrigin].TimelinePosition < runTime && Keyframes[LastTarget].TimelinePosition > runTime)
+			return (true, LastOrigin, LastTarget);
+
+		return (false, -1, -1);
+	}
+
+	private (bool valid, int origin, int target) GetSequentialKeyframes(float runTime) {
+		int i = LastLookupTime < runTime ? LastLookupKeyframe : 1;
+		LastLookupTime = runTime;
+
+		for (int j = i; j < KeyframeCount; j++)
+			if (Keyframes[j].TimelinePosition > runTime) {
+				LastLookupKeyframe = j;
+				return (true, j-1, j);
+			}
+		return (false, -1, -1);
+	}
+	private (bool valid, int origin, int target) GetKeyframes(float runTime) {
+		int origin = -1, target = -1;
+		for(int i = 0; i < KeyframeCount; i++) 
+			if(Keyframes[i].TimelinePosition < runTime) 
+				origin = i;
+
+		for(int j = KeyframeCount - 1; j >= 0; j--)
+			if(Keyframes[j].TimelinePosition > runTime)
+				target = j;
+
+		return (!(origin == -1 || target == -1), origin, target);
+	}
+}
+public struct BonesAnimation {
+	public required List<BoneAnimationKeyframe> Keyframes; 
+	public required List<AnimationBlend> FrameBlends;
+	public required float TotalDuration; // Complete duration of animation, in seconds
+	public required BonesAnimationType Type; // should transforms be treated as absolute, or relative to current position (canned anim. vs. procedural)
+}
+public enum BonesAnimationType {
+	Static,
+	Relative,
+	Custom // prep for IoC 
+}
+public struct BoneAnimationKeyframe {
+	public required BoneNode Bone;
+	public required Transform TransformState;
+	public required float TimelinePosition; // Relative to TotalDuration, 0 = at the beginning of the animation, = Duration at the end
+}
+public struct AnimationBlend {
+	public required AnimationBlendType BlendType;
+
+	public required int OriginIndex; // ref to BoneAnimationKeyframe
+	public required int TargetIndex; // ref to BoneAnimationKeyframe
+
+	public required float BlendFactor; // use blendfactor as scalar/exponent/log for linear/exponential/log movement s=vt => v=v*x, v^x, logx(v) etc
+	public required float TimeFactor; // Slow motion essentially. s=vt => t = t*TimeFactor
+	public required float Time; // how long should this take to complete
+}
+public enum AnimationBlendType {
+	None,
+	Linear,
+	Exponential,
+	Logarithmic,
+	Oscillating,
+	Custom // prep for IoC
+}
---
+++ BoneUtils/Entity/Skeleton/Animation/SkeletonAnimator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoneUtils.Entity.Skeleton.Animation; 
+public class SkeletonAnimator {
+	// Manages animation state
+
+	public required SkeletonEntity Skeleton;
+
+	private List<SkeletonAnimation> animations = [];
+	private int animationCount = 0;
+
+	public bool Running = false;
+	private float runtime = 0;
+
+	// Animation loading/unloading
+
+	public void Load(SkeletonAnimation animation) {
+		// TODO validate animation
+
+		animations.Add(animation);
+		animationCount = animations.Count;
+	}
+	public void Unload(SkeletonAnimation animation) {
+		// TODO pre-remove cleanup?
+		animations.Remove(animation);
+	}
+	public void Clear() {
+		animations.Clear();
+		animationCount = 0;
+	}
+
+	// Playback
+
+	public void Play(float deltaTime){
+		if(!Running) Running = !Running;
+		runtime += deltaTime;
+		bool valid;
+		BoneNode? node;
+		Transform? xfm;
+		for(int i = 0; i < animationCount; i++) {
+			(valid, node, xfm) = animations[i].GetKeyframe(runtime);
+			if (valid) {
+				node!.Transform = xfm!;
+			}
+		}
+	}
+	public void Scrub(float timelinePoint) {
+
+	}
+}
