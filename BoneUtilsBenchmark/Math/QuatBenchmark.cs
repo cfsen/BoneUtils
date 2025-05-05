66296bfea054b63ff1c2d14c16370024c201176c
cfsen <103598332+cfsen@users.noreply.github.com>
2025-05-05 10:42:24 +0200

    Add BenchmarkDotNet project with Quat vs native benchmarks

---
+++ BoneUtilsBenchmark/Math/QuatBenchmark.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Numerics;
+
+using BoneUtils.Math;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+
+namespace BoneUtilsBenchmark.Math;
+
+public class QuatBenchmark {
+	private Quat q0, q1;
+	private Quaternion n0, n1;
+
+	[GlobalSetup]
+	public void Setup() {
+		q0 = new(){ W=0, X=1, Y=0, Z=0 };
+		q1 = new(){ W=0, X=0, Y=1, Z=0 };
+		n0 = new(){ W=0, X=1, Y=0, Z=0 };
+		n1 = new(){ W=0, X=0, Y=1, Z=0 };
+	}
+	[BenchmarkCategory("QuatMultiplicationBench"), Benchmark]
+	public Quat QuatMultiplication_Quat() {
+		return q0*q1;
+	}
+	[BenchmarkCategory("QuatMultiplicationBench"), Benchmark]
+	public Quaternion QuatMultiplication_Native() {
+		return n0*n1;
+	}
+	[BenchmarkCategory("QuatCreate"), Benchmark]
+	public Quat QuatCreate_Quat() {
+		return new Quat();
+	}
+	[BenchmarkCategory("QuatCreate"), Benchmark]
+	public Quaternion QuatCreate_Native() {
+		return new Quaternion();
+	}
+}
---
+++ BoneUtilsBenchmark/Program.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+class Program {
+	static void Main(string[] args) {
+		Console.WriteLine("Pick benchmark to run:");
+		Console.WriteLine("1. Quat Multiplication");
+		Console.WriteLine("2. Quat Creation");
+		Console.WriteLine("3. Run ALL");
+		Console.Write("Choice: ");
+		var choice = Console.ReadLine();
+
+		string filter = choice switch {
+			"1" => "*QuatMultiplication*",
+			"2" => "*QuatCreate*",
+			"3" => "*",
+			_ => "*", // fallback to all
+		};
+
+		BenchmarkSwitcher
+			.FromAssembly(typeof(Program).Assembly)
+			.Run(new[] { "--filter", filter });
+	}
+}
