13bb01fe535e0ceaa457fbefee1983d9be04d402
cfsen <103598332+cfsen@users.noreply.github.com>
2025-05-06 20:39:01 +0200

    Add transform delegate structure to animator

--- BoneUtils/Entity/Skeleton/Animation/SkeletonAnimator.cs
+++ BoneUtils/Entity/Skeleton/Animation/SkeletonAnimator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,13 @@ public class SkeletonAnimator {
 
 	public bool Running = false;
 	private float runtime = 0;
+
+	// Hoisted from Play() to reduce allocations
+	private bool _valid;
+	private BoneNode? _node;
+	private TransformSnapshot? _xfm;
+
+	public delegate void KeyframeTransformer(BoneNode bone, TransformSnapshot xfm, AnimationXfmType ac);
 
 	public int LoadedAnimations => animationCount;
 
@@ -37,22 +45,41 @@ public class SkeletonAnimator {
 
 	// Playback
 
-	public void Play(float deltaTime){
-		if(!Running) Running = !Running;
-		runtime += deltaTime;
-		bool valid;
-		BoneNode? node;
-		TransformSnapshot? xfm;
+	public void Scrub(float timelinePoint, KeyframeTransformer? xfmHandler = null) {
+		xfmHandler ??=KeyframeTransformerBasic;
+		
 		for(int i = 0; i < animationCount; i++) {
-			(valid, node, xfm) = animations[i].GetKeyframe(runtime);
-			if (valid) {
-				node!.Transform.Position = xfm!.Value.Position;
-				node!.Transform.Rotation = xfm!.Value.Rotation;
-				node!.Transform.Scale = xfm!.Value.Scale;
-			}
+			(_valid, _node, _xfm) = animations[i].GetKeyframe(runtime);
+			
+			if (_valid && _node != null && _xfm.HasValue)
+				xfmHandler(_node, _xfm.Value, animations[i].Animation.Type);
 		}
 	}
-	public void Scrub(float timelinePoint) {
 
+	public void Play(float deltaTime, KeyframeTransformer? xfmHandler = null){
+		if(!Running) Running = !Running;
+		runtime += deltaTime;
+
+		Scrub(runtime, xfmHandler);
+	}
+
+	// TODO consider moving this out of animator
+	/// <summary>
+	/// Basic transformer for keyframes. Leverages built-ins for propagation.
+	/// </summary>
+	/// <param name="bone">BoneNode to mutate.</param>
+	/// <param name="xfm">Transform to set.</param>
+	public void KeyframeTransformerBasic(BoneNode bone, TransformSnapshot xfm, AnimationXfmType animType) {
+		bone.Transform.Scale = xfm.Scale;
+
+		if(animType == AnimationXfmType.Relative) {
+			// Leverage BoneNode propagation
+			bone.Rotate(xfm.Rotation);
+			bone.Translate(xfm.Position);
+		}
+		else if(animType == AnimationXfmType.Static) {
+			bone.Transform.Rotation = xfm.Rotation;
+			bone.Transform.Position = xfm.Position;
+		}
 	}
 }
