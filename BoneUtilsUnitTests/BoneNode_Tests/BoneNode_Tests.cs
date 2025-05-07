fecdaf2a2293c0272921c832835a0b1f582baaa8
cfsen <103598332+cfsen@users.noreply.github.com>
2025-05-07 09:43:26 +0200

    Add test for BoneNode reset

--- BoneUtilsUnitTests/BoneNode_Tests/BoneNode_Tests.cs
+++ BoneUtilsUnitTests/BoneNode_Tests/BoneNode_Tests.cs
@@ -24,4 +24,32 @@ public class BoneNode_Tests :MockDataBuilder{
 		Assert.IsTrue(node.Transform.Rotation == Quat.Identity);
 		Assert.IsTrue(node.Transform.Scale == Vector3.One);
 	}
+	[TestMethod]
+	public void BoneNode_CanReset() {
+		BoneNode node = Mock_BoneNode();
+		BoneNode identical_node = Mock_BoneNode();
+
+		Vector3 newPosition = new(1,2,3), newScale = new(2,2,2);
+		Quat newOrientation = Quat.Create(MathF.PI/2, Vector3.UnitY);
+
+		node.Rotate(Quat.Create(MathF.PI/2, Vector3.UnitY));
+		node.Translate(newPosition);
+		node.Transform.Scale = newScale;
+		
+		// TODO write a checker that accounts for rounding in Quat multiplication
+		Quat expected_orientation = new (0.7071068f, 0f, 0.7071068f, 0f);
+
+		// Check that node moved
+		Assert.AreEqual(newPosition, node.Transform.Position, "Node should move");
+		Assert.AreEqual(expected_orientation, node.Transform.Rotation, "Node should rotate");
+		Assert.AreEqual(newScale, node.Transform.Scale, "Node should scale");
+
+		node.Reset();
+
+		// Check for reset
+		Assert.AreEqual(identical_node.Transform.Position, node.Transform.Position, "Transform position should reset");
+		Assert.AreEqual(identical_node.Transform.Rotation, node.Transform.Rotation, "Transform rotatino should reset");
+		Assert.AreEqual(identical_node.Transform.Scale, node.Transform.Scale, "Transform scale should reset");
+
+	}
 }
