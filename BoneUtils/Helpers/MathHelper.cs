50dc98fad4ac64c3b68cdcded8e49fe8b8131c91
cfsen <103598332+cfsen@users.noreply.github.com>
2025-05-15 12:12:57 +0200

    Add inline XML docs to MathHelper - Remove unused code

--- BoneUtils/Helpers/MathHelper.cs
+++ BoneUtils/Helpers/MathHelper.cs
@@ -8,21 +8,11 @@ namespace BoneUtils.Helpers;
 	https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Quaternion_to_Euler_angles_(in_3-2-1_sequence)_conversion
 */
 public static class MathHelper {
-	[Obsolete("Moving off the native Quaternion type. Use Quat instead.")]
-	public static Vector3 QuaternionToEuler(Quaternion q) {
-		return new Vector3 {
-			X = MathF.Atan2(
-				2 * (q.W * q.X + q.Y * q.Z),
-				1 - 2 * (q.X * q.X + q.Y * q.Y)),
-			Y = 2 * MathF.Atan2(
-				MathF.Sqrt(1 + 2 * (q.W * q.Y - q.X * q.Z)),
-				MathF.Sqrt(1 - 2 * (q.W * q.Y - q.X * q.Z))
-				) - MathF.PI / 2,
-			Z = MathF.Atan2(
-				2 * (q.W * q.Z + q.X * q.Y),
-				1 - 2 * (q.Y * q.Y + q.Z * q.Z)),
-		};
-	}
+	/// <summary>
+	/// Converts a Quat to euler angles
+	/// </summary>
+	/// <param name="q">Quat to convert</param>
+	/// <returns>Vector3 of angles in radians</returns>
 	public static Vector3 QuatToEuler(Quat q) {
 		return new Vector3 {
 			X = MathF.Atan2(
@@ -37,40 +27,13 @@ public static class MathHelper {
 				1 - 2 * (q.Y * q.Y + q.Z * q.Z)),
 		};
 	}
+
 	/// <summary>
-	/// Determines angles of a Vector3 in its component R2 planes.
+	/// Clamps the length of a Vector3.
 	/// </summary>
-	/// <param name="u">Vector to deconstruct</param>
-	/// <param name="degrees">Return result in degrees</param>
-	/// <returns>Vector3 containing angles.</returns>
-	public static Vector3 VectorAngles(Vector3 u) {
-		Vector3 len = new() {
-			X = MathF.Max(MathF.Sqrt(u.X * u.X + u.Z * u.Z), float.Epsilon),
-			Y = MathF.Max(MathF.Sqrt(u.Y * u.Y + u.Z * u.Z), float.Epsilon),
-			Z = MathF.Max(MathF.Sqrt(u.X * u.X + u.Y * u.Y), float.Epsilon)
-		};
-
-		Vector3 s = new() { X=float.Sign(u.X), Y=float.Sign(u.Y), Z=float.Sign(u.Z) };
-
-		// Initial angle calc
-		Vector3 o = new() {
-			X = MathF.Acos(u.X / len.X),    // XZ-plane maps to rotation around Y axis, yaw = cos(x) || sin(z)
-			Y = MathF.Acos(u.Z / len.Y),    // YZ-plane maps to rotation around X axis, pitch = cos(z) || sin(y)
-			Z = MathF.Acos(u.X / len.Z)     // XY-plane maps to rotation around Z axis, roll = cos(x) || sin(y)
-		};
-
-		// Adjust for quad
-		if (s.Y >= 0 && s.Z >= 0 || s.Y < 0 && s.Z >= 0)
-			o.X *= -1.0f;
-		if (s.Y < 0 && s.Z >= 0 || s.Y < 0 && s.Z < 0)
-			o.Z *= -1.0f;
-		if (s.X < 0) {
-			o.X += MathF.PI;
-			o.X *= -1.0f;
-		}
-
-		return o;
-	}
+	/// <param name="position">Vector3 to clamp</param>
+	/// <param name="clampLength">Length to clamp at</param>
+	/// <returns>Clamped Vector.</returns>
 	public static Vector3 ClampToLength(Vector3 position, float clampLength) {
 		ArgumentOutOfRangeException.ThrowIfNegativeOrZero(clampLength);
 
@@ -82,15 +45,26 @@ public static class MathHelper {
 
 		return position*(clampLength/length);
 	}
+
+	/// <summary>
+	/// Rotates a Vector3 using a Quat and origin Vector3, correcting for floating point errors.
+	/// </summary>
+	/// <param name="position">Head of vector</param>
+	/// <param name="rotation">Rotation to apply</param>
+	/// <param name="origin">Origin of vector</param>
+	/// <returns>Reoriented vector</returns>
 	public static Vector3 RotateWithDriftCorrection(Vector3 position, Quat rotation, Vector3 origin) {
 		Vector3 u = Quat.RotateVector(rotation, position-origin);
 		return FPCorrection(u, (position-origin).Length())+origin;
 	}
-	[Obsolete("Moving off the native Quaternion type. Use Quat instead.")]
-	public static Vector3 RotateWithDriftCorrection(Vector3 childPosition, Quaternion newOrientation) {
-		Vector3 u = Vector3.Transform(childPosition, newOrientation);
-		return FPCorrection(u, childPosition.Length());
-	}
+
+	/// <summary>
+	/// Corrects for floating point errors by rounding at 6 decimal points and adjusting the vectors
+	/// length with scalar multiplication.
+	/// </summary>
+	/// <param name="u">Vector to correct.</param>
+	/// <param name="length">Intended length of vector.</param>
+	/// <returns>Corrected vector.</returns>
 	public static Vector3 FPCorrection(Vector3 u, float length) {
 		#warning Fix edge case in FPCorrection
 		// TODO edge case: corrections overshooting when provided length is less than len*0.9
@@ -101,6 +75,7 @@ public static class MathHelper {
 			u += u*(length-u.Length());
 		return u;
 	}
+
 	/// <summary>
 	/// Create a list of Vector3 that indicate the orientation of a quaternion
 	/// </summary>
@@ -120,7 +95,6 @@ public static class MathHelper {
 	public static float RadToDeg(float rad) => rad*180.0f/MathF.PI;
 	public static double DegToRad(double deg) => deg/180.0f*MathF.PI;
 	public static double RadToDeg(double rad) => rad*180.0f/MathF.PI;
-
 
 	// Structs
 
